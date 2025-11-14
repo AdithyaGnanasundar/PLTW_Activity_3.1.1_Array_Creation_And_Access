97e808c1404d85d181d7647142bc050de6eeeb61
AdithyaGnanasundar <gnanasundar2992@mydusd.org>
2025-11-13 20:58:36 -0800

    Create Dog.java

---
+++ DogArrays/Dog.java
@@ -0,0 +1,24 @@
+public class Dog {
+    private String name;
+    
+    public Dog() {
+        // no-arg constructor
+    }
+    
+    public Dog(String n) {
+        name = n;
+    }
+    
+    public void setName(String n) {
+        name = n;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void sit(int duration) {
+        // sit method
+    }
+}
+
